1d77b7eba08a2729c356823866647b84b0e483f8
dimiliakop <dai18127@uom.edu.gr>
2021-07-09 00:18:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/assignment2020_21/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.assignment2020_21;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LoginButton b;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b.findViewById(R.id.b);
+
+        callbackManager = CallbackManager.Factory.create();
+        b.setPermissions(Arrays.asList("email", "user_birhday"));
+        b.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker t = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken == null) {
+
+            }
+        }
+    };
+
+
+}
